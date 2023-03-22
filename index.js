b4c4823fce3ad01cd5eafc24d23d83d9f7f1c50f
arupsarkar <sarkar.arup@gmail.com>
2023-03-22 11:01:15 -0400

    Added twio sms capability

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const moment = require("moment");
+const app = express();
+const port = 3000;
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+require("dotenv").config();
+const client = require("twilio")(
+    process.env.TWILIO_ACCOUNT_SID,
+    process.env.TWILIO_AUTH_TOKEN
+);
+
+
+app.get("/sms", async (req, res) => {
+    console.log(process.env.TWILIO_ACCOUNT_SID);
+    console.log(process.env.TWILIO_AUTH_TOKEN);
+    console.log(process.env.TWILIO_PHONE_NUMBER);
+    const currentDateTime = moment().format("MMMM Do YYYY HH:mm:ss a");
+    await client.messages
+
+        .create({
+            body: `${currentDateTime} Good News, there is a 15% discount on all products this week`,
+            from: process.env.TWILIO_PHONE_NUMBER,
+            to: "+16462697876"
+        })
+        .then(message => {
+            console.log(message.sid)
+            res.send(message.sid);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+        .finally(() => {
+            res.send("Message sent");
+        });
+
+
+
+
+
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
