75d90070dd9a6627e12e5ab54b7ab9a8e55bdfb1
arupsarkar <sarkar.arup@gmail.com>
2023-03-22 12:02:15 -0400

    Added dynamic port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const moment = require("moment");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 
 app.use(express.json());
